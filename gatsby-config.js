11fe2e07e30af4a681f3976350ddf5653b751a7d
Brad Wray <bradwray@gmail.com>
2019-01-07 08:36:15 -0500

    Initial commit

---
+++ gatsby-config.js
@@ -0,0 +1,34 @@
+module.exports = {
+  siteMetadata: {
+    title: `Brad Wray`,
+    description: `ideas nerd, psych & comp sci teacher, creator of http://xamplr.com , ReactJS developer, edTech entrepreneur, gardener, musician, dad.`,
+    author: `@bradwray`,
+  },
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Brad Wray`,
+        short_name: `bradwray`,
+        start_url: `/`,
+        background_color: `#663399`,
+        theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+      },
+    },
+    // this (optional) plugin enables Progressive Web App + Offline functionality
+    // To learn more, visit: https://gatsby.app/offline
+    // 'gatsby-plugin-offline',
+  ],
+}
---
+++ src/pages/index.js
@@ -0,0 +1,45 @@
+import React from 'react'
+import Layout from '../components/layout'
+import SEO from '../components/seo'
+
+const IndexPage = () => (
+  <Layout>
+    <SEO
+      title="Home"
+      keywords={[`developer`, `teacher`, `edTech`, `musician`, `xamplr`]}
+    />
+    <br />
+    <a href="https://www.xamplr.com/">
+      Xamplr: An app for learning vocabulary with examples
+    </a>
+    <br />
+    <a href="https://github.com/bradwray">Github profile</a>
+    <br />
+    <a href="https://www.linkedin.com/in/brad-wray-951b178">LinkedIn profile</a>
+    <br />
+    <a href="https://codesandbox.io/u/bradwray">CodeSandbox projects</a>
+    <br />
+    <a href="https://twitter.com/BradWray">Twitter profile</a>
+    <br />
+    <a href="https://www.youtube.com/user/wraybm1">Youtube channel</a>
+    <br />
+    <a href="https://bradwray.bandcamp.com/">Bandcamp albums</a>
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+  </Layout>
+)
+
+export default IndexPage
